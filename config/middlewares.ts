bd45ca0f7e48eb895bc380849a978fbd87a02da3
aprilswind <joel.sr1024@gmail.com>
2023-03-18 21:33:08 +0530

    Fix: cors error

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,4 +9,13 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'http://localhost:1337', 'https://server.codingclubgct.in', 'https://verify.codingclubgct.in'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
 ];
