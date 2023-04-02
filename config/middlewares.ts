7047a2d08fc489c6d66183eec53ba1d9073b2b60
Jeremy Asirwaad <jeremyasirwaad@Jeremys-MacBook-Air.local>
2023-04-03 00:10:44 +0530

    cors fix

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -12,7 +12,7 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000', 'http://localhost:1337', 'https://server.codingclubgct.in', 'https://verify.codingclubgct.in', 'https://codingclub-pj6t6.ondigitalocean.app'],
+      origin: ['*'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
