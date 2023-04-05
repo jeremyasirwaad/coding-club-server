6c6f24e20984e78d94c10301c6c7572e508bae44
Jeremy Asirwaad <jeremyasirwaad@Jeremys-MacBook-Air.local>
2023-04-06 01:40:03 +0530

    lifecycles made for Job-opportunities

---
+++ src/api/job-opportunity/content-types/job-opportunity/lifecycles.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const requestOptions = {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json', 'Authorization' : 'key=AAAAjOaXe4w:APA91bFN1tW_7Ol4aQg9ydD28zX8nzMr6qYj6HF27wtfctE6muF-oT9dPlt5GFuVaxgsUI1OZ92p4fuoFliJp7_P_TP4YZxXBC2RO7fdvKE1eVMMqBVyAwI1iZ0Ens7Lv8PViZqSWbNA'  },
+  body: JSON.stringify({
+    "to": "/topics/NOTE",
+    "notification": {
+      "title": "Check this Mobile (title)",
+      "body": "Rich Notification testing (body)"
+      }
+})
+};
+
+
+export default {
+  async afterCreate(event: any) {
+    const { result } = event
+    await axios.post('https://fcm.googleapis.com/fcm/send',{
+      "to": "/topics/NOTE",
+      "notification": {
+        "title": "New Job Opportunity",
+        "body": result.JobTitle,
+        "sound" : "default"
+        },
+        "data": {
+          "url": result.Job_Poster
+            }
+  }  ,{headers: {
+      Authorization: 'key=AAAAjOaXe4w:APA91bFN1tW_7Ol4aQg9ydD28zX8nzMr6qYj6HF27wtfctE6muF-oT9dPlt5GFuVaxgsUI1OZ92p4fuoFliJp7_P_TP4YZxXBC2RO7fdvKE1eVMMqBVyAwI1iZ0Ens7Lv8PViZqSWbNA',
+    }}).then(res => console.log(res.data))
+  }
+}
