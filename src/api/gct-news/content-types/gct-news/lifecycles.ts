d2b964a81970982bc3ae1767ef42b78ab2f38b3f
Jeremy Asirwaad <jeremyasirwaad@Jeremys-MacBook-Air.local>
2023-04-07 20:07:33 +0530

    added sound to notification

--- src/api/gct-news/content-types/gct-news/lifecycles.ts
+++ src/api/gct-news/content-types/gct-news/lifecycles.ts
@@ -9,8 +9,11 @@ export default {
         "title": "News -" + result.AppTitle,
         "body": result.AppDesc,
         "sound" : "default"
-
         },
+        "android": {
+          "priority": "high"
+        },
+       "priority": 10
        
   }  ,{headers: {
       Authorization: 'key=AAAAjOaXe4w:APA91bFN1tW_7Ol4aQg9ydD28zX8nzMr6qYj6HF27wtfctE6muF-oT9dPlt5GFuVaxgsUI1OZ92p4fuoFliJp7_P_TP4YZxXBC2RO7fdvKE1eVMMqBVyAwI1iZ0Ens7Lv8PViZqSWbNA',
